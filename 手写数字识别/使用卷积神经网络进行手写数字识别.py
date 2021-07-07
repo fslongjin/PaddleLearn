9270217d22f783a5bb44efd962c46ab06ba38327
longjin <fslongjin@vip.qq.com>
2021-07-07 23:26:13 +0800

    修改 使用卷积神经网络进行手写数字识别 使用交叉熵损失函数以及加入softmax

--- 手写数字识别/使用卷积神经网络进行手写数字识别.py
+++ 手写数字识别/使用卷积神经网络进行手写数字识别.py
@@ -1,5 +1,5 @@
 import paddle
-from paddle.nn import Conv2D, MaxPool2D, Linear
+from paddle.nn import Conv2D, MaxPool2D, Linear, Softmax
 import paddle.nn.functional as F
 import json, gzip
 import numpy as np
@@ -53,7 +53,7 @@ def load_data_sync(mode='train'):
         for i in index_list:
             # 将数据处理成希望的类型
             img = np.array(imgs[i]).astype('float32')
-            label = np.array(labels[i]).astype('float32')
+            label = np.array(labels[i]).astype('int64')
             imgs_list.append(img)
             labels_list.append(label)
 
@@ -85,8 +85,8 @@ class MNIST(paddle.nn.Layer):
         self.conv2 = Conv2D(in_channels=20, out_channels=20, kernel_size=5, stride=1, padding=2)
         self.max_pool_2 = MaxPool2D(kernel_size=2, stride=2)
 
-        # 定义一层全连接层，输出维度为1
-        self.fc = Linear(in_features=980, out_features=1)
+        # 定义一层全连接层，输出维度为10
+        self.fc = Linear(in_features=980, out_features=10)
 
     # 定义网络前向计算过程，卷积后紧接着使用池化层，最后使用全连接层计算最终输出
     # 卷积层激活函数使用relu， 全连接层不使用激活函数
@@ -99,6 +99,7 @@ class MNIST(paddle.nn.Layer):
         x = self.max_pool_2(x)
         x = paddle.reshape(x, [x.shape[0], -1])
         x = self.fc(x)
+        x = F.softmax(x)
         return x
 
 
@@ -130,7 +131,7 @@ def train(model):
             predicts = model(images)
 
             # 计算损失，取一个批次样本损失的平均值
-            loss = F.square_error_cost(predicts, labels)
+            loss = F.cross_entropy(predicts, labels)
             avg_loss = paddle.mean(loss)
 
             # 每训练200批次的数据，打印当前loss情况
