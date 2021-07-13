d9ea3d6e3b8a54c50d67cc7c7f6507a1b486b464
longjin <fslongjin@vip.qq.com>
2021-07-13 11:58:23 +0800

    修改 使用卷积神经网络进行手写数字识别 增加了在测试集中校验准确率的代码

--- 手写数字识别/使用卷积神经网络进行手写数字识别.py
+++ 手写数字识别/使用卷积神经网络进行手写数字识别.py
@@ -234,14 +234,52 @@ def predict():
     print("本次预测的数字是：", lab[0][-1])
 
 
+def evaluation(model):
+    print("start evaluation...")
+    # 定义预测过程
+    params_file_path = "work/mnist-cnn.pdparams"
+    # 加载模型参数
+    param_dict = paddle.load(params_file_path)
+    model.load_dict(param_dict)
+
+    model.eval()
+    eval_loader = load_data_sync('eval')
+
+    acc_set = []
+    avg_loss_set = []
+
+    for batch_id, data in enumerate(eval_loader()):
+        images, labels = data
+        images = paddle.to_tensor(images)
+        labels = paddle.to_tensor(labels)
+
+        predicts, acc = model(images, labels)
+        loss = F.cross_entropy(input=predicts, label=labels)
+        avg_loss = paddle.mean(loss)
+        acc_set.append(float(acc.numpy()))
+        avg_loss_set.append(float(avg_loss.numpy()))
+
+    # 计算多个batch的平均损失和准确率
+    acc_val_mean = np.array(acc_set).mean()
+    avg_loss_mean = np.array(avg_loss_set).mean()
+
+    print("loss={}, acc={}".format(avg_loss_mean, acc_val_mean))
+
+
+
+
 if __name__ == '__main__':
 
-    mode = 'train'
+    mode = 'eval'
     if mode == 'train':
         model = MNIST()
         train(model)
     elif mode == 'predict':
         predict()
+    elif mode == 'eval':
+        model = MNIST()
+        evaluation(model)
+
 
 
 
